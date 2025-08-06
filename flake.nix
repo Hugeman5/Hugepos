c0c8cdfdae3889530e877d6968eae79c98cca0d9
test <test@google.com>
2025-08-06 05:20:26 +0000

    Add flake.nix for reproducible development environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A development environment for the HugePOS project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.nodejs_20
+        pkgs.firebase-tools
+        pkgs.gh
+        pkgs.direnv
+        pkgs.zulu
+      ];
+
+      env = {
+        NODE_ENV = "development";
+      };
+    };
+  };
+}
